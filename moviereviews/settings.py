b8c56fbb0de88d53455bdce344536d262788abdf
Daniel Correa <yo@danielgara.com>
2021-11-27 08:22:48 -0500

    imp: Chapter17

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -129,6 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'moviereviews/static/',
+    BASE_DIR / 'moviereviews/static/',
 ]
